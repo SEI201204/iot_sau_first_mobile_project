87920ca28e2454bb869c290dd5f6e8c698b9b5ea
SEI201204 <s6519410036@sau.ac.th>
2025-01-15 12:25:14 +0700

    v.2 : creat  3 file welcome signup signin edit main and page  welcome already

---
+++ lib/views/welcome_ui.dart
@@ -0,0 +1,90 @@
+// ignore_for_file: sort_child_properties_last
+
+import 'package:flutter/material.dart';
+
+class welcomeUI extends StatefulWidget {
+  const welcomeUI({super.key});
+
+  @override
+  State<welcomeUI> createState() => _welcomeUIState();
+}
+
+class _welcomeUIState extends State<welcomeUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
+            Image.asset(
+              'assets/images/logo.png',
+              //width: 250,
+              width: MediaQuery.of(context).size.width * 0.7,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.2,
+            ),
+            Text(
+              'IoT SAU APP',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.03,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text('Welcome to IoT SAU APP'),
+            Text('Created by Team V'),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                      7.0,
+                    )),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.1,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'SIGNUP',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(100, 35),
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                      7.0,
+                    )),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
